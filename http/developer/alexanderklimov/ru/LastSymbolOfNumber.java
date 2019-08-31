1aee0c329029ed7aeced613ce303327988124169
nikolajred <nikolajred@gmail.com>
2019-08-31 15:45:28 +0300

    second commit

--- http/developer/alexanderklimov/ru/LastSymbolOfNumber.java
+++ http/developer/alexanderklimov/ru/LastSymbolOfNumber.java
@@ -3,24 +3,37 @@ package http.developer.alexanderklimov.ru;
 import java.util.Scanner;
 
 public class LastSymbolOfNumber {
-   public static int yourNumber;
+   public static long yourNumber;
    public static char lastSymbolOfYourNumber;
 
     public static void main(String[] args) {
+
+        createYourNumber();
+        searchLastSymbolOfYourNumber();
+
+    }
+
+    public static long createYourNumber(){
         Scanner scn =new Scanner(System.in);
         System.out.println("Input your number: ");
 
-        if (scn.hasNextInt()){
-            yourNumber = scn.nextInt();
+        if (scn.hasNextLong()){
+            yourNumber = scn.nextLong();
+            return yourNumber;
         }else{
-            while(!scn.hasNextInt()){
+            while(!scn.hasNextLong()){
                 System.out.println("Your number is not number: ");
                 System.out.println("Input your number: ");
                 scn.nextLine();
             }
-        }
-
-
+        }return yourNumber;
+    }
+    public static char searchLastSymbolOfYourNumber(){
+        String yourNumberToString = Long.toString(yourNumber);
+        StringBuilder stringBuilder = new StringBuilder(yourNumberToString);
+        lastSymbolOfYourNumber = stringBuilder.charAt(stringBuilder.length()-1);
+        System.out.println("last symbol of your number is: " + lastSymbolOfYourNumber);
+        return lastSymbolOfYourNumber;
     }
 
 
